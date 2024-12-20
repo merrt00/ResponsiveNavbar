f592b67f7613713670600c4876f48327bf2ef918
Mert <88328115+merrt00@users.noreply.github.com>
2024-12-20 16:55:26 +0300

    Final

---
+++ script.js
@@ -0,0 +1,15 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    navList.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
